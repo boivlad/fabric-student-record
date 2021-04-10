0735d58c6294130c633b2db9060234f543731afb
ili2908 <illia.kovalenko@ukma.edu.ua>
2021-04-10 20:06:30 +0300

    m

--- chaincode/src/contracts/student-records-storage.js
+++ chaincode/src/contracts/student-records-storage.js
@@ -7,40 +7,84 @@ class StudentRecordsStorage extends Contract {
   constructor() {
     super('org.fabric.studentRecordsStorage');
   }
-
-  async createStudentRecord(ctx, studentEmail, fullName) {
-    const identity = new ClientIdentity(ctx.stub);
+  checkIsTeacher(ctx){
+	const identity = new ClientIdentity(ctx.stub);
     if (identity.cert.subject.organizationalUnitName !== 'teacher') {
       throw new Error('Current subject is not have access to this function');
     }
+  }
+  checkStudentExists(recordAsBytes){
+	if(!recordAsBytes || recordAsBytes.toString().length === 0){
+      throw new Error('Student with current email does not exist');
+    }
+  }
+  checkStudentNotExists(recordAsBytes){
+	if(recordAsBytes || recordAsBytes.toString().length !== 0){
+       throw new Error('Student with the current email already exist');
+    }
+  }
+  uParser(recordAsBytes){
+	 return JSON.parse(recordAsBytes.toString());
+  }
+  async createStudentRecord(ctx, studentEmail, fullName) {
+    this.checkIsTeacher(ctx);
     const recordAsBytes = await ctx.stub.getState(studentEmail);
-    // if(!recordAsBytes || recordAsBytes.toString().length !== 0){
-    //   throw new Error('Student with the current email already exist');
-    // }
+    this.checkStudentNotExists(recordAsBytes);
     const recordExample = {
       fullName: fullName,
       semesters: []
-    }
+    };
     const newRecordInBytes = Buffer.from(JSON.stringify(recordExample));
     await ctx.stub.putState(studentEmail, newRecordInBytes);
     return JSON.stringify(recordExample, null, 2);
   }
 
-  async addSubjectToStudentRecord(ctx, studentEmail, semesterNumber, subjectName) {
-    const identity = new ClientIdentity(ctx.stub);
-    if (identity.cert.subject.organizationalUnitName !== 'teacher') {
-      throw new Error('Current subject is not have access to this function');
-    }
+  async addSubject(ctx, studentEmail, semester, subject) {
+    this.checkIsTeacher(ctx);
     const recordAsBytes = await ctx.stub.getState(studentEmail);
-    const recordAsObject = JSON.parse(recordAsBytes.toString());
-    recordAsObject.semesters[semesterNumber][subjectName] = {
+	this.checkStudentExists(recordAsBytes);
+	const identity = new ClientIdentity(ctx.stub);
+    const tmp = this.uParser(recordAsBytes);
+    tmp.semesters[semester][subject] = {
       lector: identity.cert.subject.commonName,
       themes: []
-    }
-    const newRecordInBytes = Buffer.from(JSON.stringify(recordAsObject));
-    await ctx.stub.putState(studentEmail, newRecordInBytes);
-    return JSON.stringify(recordAsObject, null, 2);
+    };
+    const changes = Buffer.from(JSON.stringify(tmp));
+    await ctx.stub.putState(studentEmail, changes);
+    return JSON.stringify(tmp);
   }
+  async addGrade(ctx, studentEmail, semester, subject, theme, grade){
+    this.checkIsTeacher(ctx);
+	const recordAsBytes = await ctx.stub.getState(studentEmail);
+    this.checkStudentExists(recordAsBytes);
+	const tmp = this.uParser(recordAsBytes);
+    if(!tmp.semesters[semester]){
+      throw new Error('Semester not found');
+    }
+    else if(!tmp.semesters[semester][subject]){
+      throw new Error('No subject in semester');
+    }
+    tmp.semesters[semester][subject].themes.push({
+        title: theme,
+        rating: grade,
+        date: Date.now()
+      });
+	const changes = Buffer.from(JSON.stringify(tmp));
+	await ctx.stub.putState(studentEmail, changes);
+    return JSON.stringify(tmp);
+  }
+  
+  async getGrades(ctx, studentEmail) {
+    const recordAsBytes = await ctx.stub.getState(studentEmail);
+    this.checkStudentExists(recordAsBytes);
+    return JSON.stringify(this.uParser(recordAsBytes).semesters || {});
+  }
+
+  async getGradesSemester(ctx, studentEmail, semester) {
+    const recordAsBytes = await ctx.stub.getState(studentEmail);
+    this.checkStudentExists(recordAsBytes);
+    return JSON.stringify(this.uParser(recordAsBytes).semesters[semester] || {});
+  } 
 }
 
 module.exports = StudentRecordsStorage;
